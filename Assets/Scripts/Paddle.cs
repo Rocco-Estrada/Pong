5c846be52da17944941a8140bbc6f3f48d30d188
Rocco-Estrada <roccoestrada@gmail.com>
2020-02-09 20:11:37 -0800

    Start of Pong Part 2

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public AudioClip hitSound;
+    public AudioSource speaker;
+    public GameObject ball;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        speaker = GetComponent<AudioSource>();
+        ball = GameObject.Find("Ball");
+        rb = ball.GetComponent<Rigidbody>();
+    }
+
+    // Plays audio clip on collision as well as modifies the pitch according to
+    // the balls speed
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            speaker.PlayOneShot(hitSound);
+            GetComponent<AudioSource>().pitch += rb.velocity.magnitude / 10;
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public GameObject ball;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+        rb = ball.GetComponent<Rigidbody>();
+    }
+    // Power up makes ball speed slow down.
+    // Power down makes ball scale down to a smaller size.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Ball" && this.gameObject.name == "PowerUp")
+        {
+            rb.velocity *= 0.4f;
+            Destroy(this);
+            Destroy(GetComponent<MeshRenderer>());
+        }
+
+        if (other.gameObject.name == "Ball" && this.gameObject.name == "PowerDown")
+        {
+            ball.transform.localScale *= 0.4f;
+            Destroy(this);
+            Destroy(GetComponent<MeshRenderer>());
+        }
+
+    }
+}
